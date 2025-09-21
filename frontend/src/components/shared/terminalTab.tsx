b317118aa71c07374b127af95d2d1126f911e493
kunal <kunal.dusk@gmail.com>
2025-09-21 13:00:31 +0530

    update editor ui

--- frontend/src/components/shared/terminalTab.tsx
+++ frontend/src/components/shared/terminalTab.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { X } from "lucide-react";
-import { Button } from "./button";
-import { MouseEventHandler } from "react";
+import { Button } from "../ui/button";
 
-export default function Tab({
+export default function FileTab({
 	children,
 	saved,
 	selected,
@@ -41,13 +40,9 @@ export default function Tab({
 				}
 				className="h-5 w-5 ml-0.5 group flex items-center justify-center translate-x-1 transition-colors bg-transparent hover:bg-muted-foreground/25 cursor-pointer rounded-sm"
 			>
-				{saved ? (
-					<X className="w-3 h-3" />
-				) : (
-					<>
-						<X className="w-3 h-3 group-hover:block hidden" />
-						<div className="w-2 h-2 rounded-full bg-foreground group-hover:hidden" />
-					</>
+				<X className="w-3 h-3 group-hover:block hidden" />
+				{selected && (
+					<div className="w-2 h-2 rounded-full bg-green-400 group-hover:hidden" />
 				)}
 			</div>
 		</Button>
